9dd513ecaef879824f92d2f15281cd73bd0910a3
tlaraine <tlaraine@debian-BULLSEYE-live-builder-AMD64>
2022-12-12 15:18:36 +0300

    add_debian_box

--- bonus/scripts/debian-gitlab.sh
+++ bonus/scripts/debian-gitlab.sh
@@ -1,22 +1,28 @@
-# Yum update
-sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-# sudo yum update -y
+sudo apt-get update
+sudo apt-get install -y curl openssh-server ca-certificates perl
 
-# Gitlab dependancies
-sudo yum -y install curl vim policycoreutils python3-policycoreutils
+
+
 
 # Gitlab install
-curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
-sudo yum install gitlab-ce -y
+# curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+# sudo apt install gitlab-ce -y
+
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+
+
+sudo EXTERNAL_URL="http://192.168.56.110" apt-get install gitlab-ee
+
+
 
 # Meow
-sudo sed -i '32s/.*/external_url "http\:\/\/192.168.56.110"/' /etc/gitlab/gitlab.rb
+# sudo sed -i '32s/.*/external_url "http\:\/\/192.168.56.110"/' /etc/gitlab/gitlab.rb
 
 # Gitlab config
 # Edit configs
 # sudo vim /etc/gitlab/gitlab.rb
-sudo gitlab-ctl reconfigure
+# sudo gitlab-ctl reconfigure
 
 # Get root's default password
 sudo cat /etc/gitlab/initial_root_password
